fd5c2d17f361e6ef31ae1695b0ee6d629df411a4
Aleksandr <kuzovkov.aleksandr@rambler.ru>
2022-01-25 00:05:01 +0300

    ...

---
+++ yavitrina/yavitrina/spiders/product_parser.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from scrapy.loader import ItemLoader
+from scrapy_splash import SplashRequest
+import math
+import re
+import os
+import pkgutil
+from pprint import pprint
+import time
+import html2text
+import datetime
+import logging
+import urllib
+from yavitrina.items import CategoryItem
+from yavitrina.items import CategoryDescriptionItem
+from yavitrina.items import TagItem
+from yavitrina.items import ProductCardItem
+from yavitrina.items import ProductItem
+from yavitrina.items import ImageItem
+from yavitrina.items import SearchTagItem
+from yavitrina.items import CategoryTagItem
+from yavitrina.items import SettingItem
+from yavitrina.items import SettingValueItem
+from yavitrina.scrapestack import ScrapestackRequest
+from yavitrina.seleniumrequest import SelenuimRequest
+from scrapy_headless import HeadlessRequest
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from urllib import urlencode
+
+############################################################################
+# Parsing product pages if products is absent but product_card is present
+############################################################################
+
+class ProductParserSpider(scrapy.Spider):
+    name = 'product_parser'
+    #allowed_domains = ['yavitrina.ru', 'i.yavitrina.ru']
+    allowed_domains = []
+    dirname = 'product_parser'
+    handle_httpstatus_list = [400, 404]
+    h = html2text.HTML2Text()
+    h.ignore_emphasis = True
+    h.ignore_links = True
+    logger = logging.getLogger()
+    drain = False
+    pagination = {}
+    base_url = 'https://yavitrina.ru'
+    lua_src = pkgutil.get_data('yavitrina', 'lua/html-render.lua')
+    lua_src2 = pkgutil.get_data('yavitrina', 'lua/html-render-scrolldown.lua')
+    clear_db = False
+    db = None
+    paginations = {}
+    scrapestack_access_key = ''
+    product_request_type = 'headless'
+    request_type = 'splash'
+    #product_request_type = 'selenium'
+    use_scrapestack = True
+    custom_settings = {
+        'SELENIUM_GRID_URL': 'http://selenium-hub:4444/wd/hub',  # Example for local grid with docker-compose
+        'SELENIUM_NODES': 1,  # Number of nodes(browsers) you are running on your grid
+        'SELENIUM_CAPABILITIES': DesiredCapabilities.CHROME,
+        'DOWNLOAD_HANDLERS': {
+            "http": "scrapy_headless.HeadlessDownloadHandler",
+            "https": "scrapy_headless.HeadlessDownloadHandler",
+        },
+        #'LOG_LEVEL': 'DEBUG',
+        'LOG_LEVEL': 'INFO',
+    }
+
+    def __init__(self, drain=False, noproxy=False, cleardb=False, request_type=False, product_request_type=False, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        if drain:
+            self.drain = True
+        if noproxy:
+            self.use_scrapestack = False
+        if cleardb:
+            self.clear_db = True
+        if request_type:
+            self.request_type = request_type
+        if product_request_type:
+            self.product_request_type = product_request_type
+
+    def getRequest(self, url, callback, request_type=None, dont_filter=False):
+        url_origin = url
+        if request_type is None:
+            request_type = self.request_type
+        if self.use_scrapestack:
+            params = {'access_key': self.scrapestack_access_key, 'url': url}
+            url = 'http://api.scrapestack.com/scrape?' + urlencode(params)
+        if request_type == 'headless':
+            request = HeadlessRequest(url, callback=callback, driver_callback=self.process_webdriver)
+        elif request_type == 'selenium':
+            request = SelenuimRequest(url, callback=callback, dont_filter=dont_filter, options={'minsize': 2028, 'wait': 2})
+        elif request_type == 'scrapestack':
+            url = url_origin
+            request = ScrapestackRequest(url, callback=callback, access_key=self.scrapestack_access_key, dont_filter=dont_filter, options={'render_js': 1})
+        elif request_type == 'splash':
+            args = {'wait': 10.0, 'lua_source': self.lua_src, 'timeout': 3600}
+            request = SplashRequest(url, callback=callback, endpoint='execute', args=args, meta={"handle_httpstatus_all": True}, dont_filter=dont_filter)
+            request.meta['url_origin'] = url_origin
+        else:
+            request = scrapy.Request(url, callback=callback, dont_filter=dont_filter)
+        if self.use_scrapestack:
+            request.meta['url_origin'] = url_origin
+        return request
+
+    def start_requests(self):
+        url = 'http://localhost:6800'
+        request = scrapy.Request(url, callback=self.handle_product_cards)
+        yield request
+
+    def handle_product_cards(self, response):
+        total_cards = self.db._getone("SELECT count(*) AS total FROM product_card pc LEFT JOIN  product p ON pc.product_id = p.product_id WHERE p.title ISNULL")
+        self.logger.info('total_cards; {total}'.format(total=total_cards))
+        LIMIT = 100
+        offsets = range(0, total_cards, LIMIT)
+        for offset in offsets:
+            self.logger.debug('handle tags %s-%s' % (offset, min(offset+LIMIT, total_cards)))
+            cards = self.db._getraw("SELECT pc.url, pc.product_id, pc.html AS total FROM product_card pc LEFT JOIN  product p ON pc.product_id = p.product_id WHERE p.title ISNULL ORDER BY pc.id OFFSET {offset} LIMIT {limit} ".format(offset=offset, limit=LIMIT), ['url', 'product_id', 'html'])
+            for card in cards:
+                pprint(card['url'])
+                # url = ''.join([self.base_url, card['url']])
+                # request = self.getRequest(url, callback=self.handle_target_page, request_type='origin')
+                # yield request
+
+    def process_webdriver(self, driver):
+        IMPLICITLY_WAIT = 3
+        driver.implicitly_wait(IMPLICITLY_WAIT)
+        time.sleep(IMPLICITLY_WAIT)
+
+
+    def get_request_url(self, response):
+        if 'url_origin' in response.meta:
+            return response.meta['url_origin']
+        elif hasattr(response.request, 'url_origin'):
+            return response.request.url_origin
+        else:
+            return response.request.url
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
